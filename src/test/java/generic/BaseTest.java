b1c7c30334e90916c98c5abbd1eb34861856cb62
Prashanth <Prashanth@DESKTOP-7QJBVRE>
2023-06-21 11:16:00 +0530

    added properties

--- src/test/java/generic/BaseTest.java
+++ src/test/java/generic/BaseTest.java
@@ -1,7 +1,9 @@
 package generic;
 
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,64 +11,134 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.support.events.EventFiringDecorator;
 
+
+@Listeners(TestListener.class)
 public class BaseTest {
-	public static final String DEFAULT_URL="https://demo.actitime.com/login.do";
+	public static final String DEFAULT_URL="https://demo.actitime.com";
 	public static final String DEFAULT_GRID="no";
-	public static final String DEFAULT_BROWSER="chrome";
-	public static final String XL_PATH="./data/input.xlsx";
+	public static final String DEFAULT_PPTFILE="qa.properties";
+	public static final String DEFAULT_HTMLPATH="report/Spark.html";
+	public static String XL_PATH;
+	public static ExtentReports report;
+	public ExtentTest extentTest;
 	
+	public WebDriver original_driver;
 	public WebDriver driver;
 	public WebDriverWait wait;
+
+	@Parameters({"htmlpath"})
+	@BeforeSuite
+	public void intReport(@Optional(DEFAULT_HTMLPATH)  String htmlPath)
+	{
+		report=new ExtentReports();	
+		ExtentSparkReporter spark = new ExtentSparkReporter(htmlPath);
+		report.attachReporter(spark);
+	}
 	
-	@Parameters({"appurl","grid","browser"})
+	@AfterSuite
+	public void generateReport()
+	{
+		report.flush();
+	}
+	
+	@Parameters({"appurl","grid","pptfile"})
 	@BeforeMethod
-	public void preCondition(@Optional(DEFAULT_URL) String appURL,@Optional(DEFAULT_GRID) String grid,@Optional(DEFAULT_BROWSER) String browser) throws Exception {
+	public void preCondition(@Optional(DEFAULT_URL) String appURL,@Optional(DEFAULT_GRID) String grid,@Optional(DEFAULT_PPTFILE) String pptFile,Method method) throws Exception {
+		String testName=method.getName();
+		extentTest = report.createTest(testName);
 		
+		String browser=Util.getProperty(pptFile, "browser");
+		extentTest.info("Browser is:"+browser);
+		
+		String sITO=Util.getProperty(pptFile, "ITO");
+		long lITO=Long.parseLong(sITO);
+		extentTest.info("ITO is:"+sITO);
+		
+		String sETO=Util.getProperty(pptFile, "ETO");
+		long lETO=Long.parseLong(sETO);
+		extentTest.info("ETO is:"+sETO);
+		
+		XL_PATH=Util.getProperty(pptFile, "XLPATH");
+		extentTest.info("XL PATH is:"+XL_PATH);
+
 		if(grid.equalsIgnoreCase("no"))
 		{
-			Reporter.log("Execution in local system",true);
+			extentTest.info("Execution in local system");
 			if(browser.equals("chrome"))
 			{
-				Reporter.log("Browser is: chrome",true);
-				driver=new ChromeDriver();
+				extentTest.info("Browser is: chrome");
+				original_driver=new ChromeDriver();
 			}
 			else
 			{
-				Reporter.log("Browser is: Firefox",true);
-				driver=new FirefoxDriver();
+				extentTest.info("Browser is: Firefox");
+				original_driver=new FirefoxDriver();
 			}
 		}
 		else
 		{
-			Reporter.log("Execution in remote system",true);
+			extentTest.info("Execution in remote system");
 			if(browser.equals("chrome"))
 			{
-				Reporter.log("Browser is: chrome",true);
-				driver=new RemoteWebDriver(new URL(grid), new ChromeOptions());
+				extentTest.info("Browser is: chrome");
+				original_driver=new RemoteWebDriver(new URL(grid), new ChromeOptions());
 			}
 			else
 			{
-				Reporter.log("Browser is: Firefox",true);
-				driver=new RemoteWebDriver(new URL(grid), new FirefoxOptions());
+				extentTest.info("Browser is: Firefox");
+				original_driver=new RemoteWebDriver(new URL(grid), new FirefoxOptions());
 			}
 			
 		}
-		driver.get(appURL);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		extentTest.info("Enter the URL:"+appURL);
+		original_driver.get(appURL);
+		extentTest.info("maximize the browser");
+		original_driver.manage().window().maximize();
+		extentTest.info("Set ITO to :"+lITO);
+		original_driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(lITO));
+		extentTest.info("Set ETO to "+lETO);
+		wait=new WebDriverWait(original_driver, Duration.ofSeconds(lETO));
+		EventFiringDecorator<WebDriver> decorator=new EventFiringDecorator<WebDriver>(new SeleniumListener(extentTest));
+		driver = decorator.decorate(original_driver);
 	}
 	
 	@AfterMethod
-	public void postCondition() {
+	public void postCondition(ITestResult result) {
+		
+		List<String> output = Reporter.getOutput(result);
+		for(String msg:output)
+		{
+			extentTest.info(msg);
+		}
+		
+		String testName = result.getName();
+		int status=result.getStatus();
+		if(status==2)
+		{
+			String timeStamp = Util.getTimeStamp();
+			String imgPath="./images/"+testName+timeStamp+".png";
+			ScreenShot.takePageScreenShot(driver,imgPath);
+			String msg=result.getThrowable().getMessage();
+			imgPath="../images/"+testName+timeStamp+".png";
+			extentTest.fail(msg,MediaEntityBuilder.createScreenCaptureFromPath(imgPath).build());
+		}
+		extentTest.info("Closing the browser");
 		driver.quit();
 	}
 }
