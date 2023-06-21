ac0bb60bdbec5702015fd3700bc896a5733beedd
Prashanth <Prashanth@DESKTOP-7QJBVRE>
2023-06-21 13:08:25 +0530

    util changes

--- src/test/java/generic/Util.java
+++ src/test/java/generic/Util.java
@@ -1,11 +1,18 @@
 package generic;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.openqa.selenium.By;
 
 public class Util {
@@ -50,5 +57,72 @@ public class Util {
 		
 		}
 		return msg;
+	}
+	
+	public static String[][] getDataFromCSVtoArray(String path)
+	{
+		String[][] data=null;
+		
+		try {
+				FileReader f=new FileReader(path);
+				CSVParser csvParser = CSVParser.parse(f,CSVFormat.DEFAULT);
+				List<CSVRecord> allRecords = csvParser.getRecords();
+				
+				int rowCount=allRecords.size();
+				int colCount=allRecords.get(0).size();
+				
+				data=new String[rowCount-1][colCount];
+				
+				for(int i=1;i<rowCount;i++)
+				{
+					CSVRecord record = allRecords.get(i);
+					
+					for(int j=0;j<colCount;j++)
+					{
+						String v = record.get(j);
+						data[i-1][j]=v;
+					}
+		
+				}
+				csvParser.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return data;
+	}
+	
+	public static Iterator<String[]> getDataFromCSVtoIterator(String path)
+	{
+		ArrayList<String[]> data=new ArrayList<String[]>();
+		
+		try {
+				FileReader f=new FileReader(path);
+				CSVParser csvParser = CSVParser.parse(f,CSVFormat.DEFAULT);
+				List<CSVRecord> allRecords = csvParser.getRecords();
+				
+				int rowCount=allRecords.size();
+				int colCount=allRecords.get(0).size();
+
+				for(int i=1;i<rowCount;i++)
+				{
+					CSVRecord record = allRecords.get(i);
+					String[] a=new String[colCount];
+					for(int j=0;j<colCount;j++)
+					{
+						String v = record.get(j);
+						a[j]=v;
+					}
+		
+					data.add(a);
+				}
+				csvParser.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		Iterator<String[]> iData = data.iterator();
+		return iData;
 	}
 }
